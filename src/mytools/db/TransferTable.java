ca684117ed2f51199b8a032e6fac446cd76fe746
Noriyuki Takarabe <junk.freak.net@gmail.com>
2015-04-11 03:29:49 +0900

    2015/04/11(sat) 03:29 commit and push on envy.

--- src/mytools/db/TransferTable.java
+++ src/mytools/db/TransferTable.java
@@ -2,21 +2,30 @@ package mytools.db;
 
 public class TransferTable {
 
-	private mycommons.db.connection.ParaConnection paraConnectionSouce;
-	private mycommons.db.Table tableSource;
-	private mycommons.db.connection.ParaConnection paraConnectionTarget;
-	private mycommons.db.Table tableTarget;
+	//private mycommons.db.connection.ParaConnection paraConnectionSouce;
+	//private mycommons.db.Table tableSource;
+	//private mycommons.db.connection.ParaConnection paraConnectionTarget;
+	//private mycommons.db.Table tableTarget;
 	/***
 	public TransferTable(mycommons.db.connection.ParaConnection in_source,mycommons.db.Table in_source_table){
 		
 	}
 	***/
 	public void transfer(mycommons.db.connection.ParaConnection in_connection_source,mycommons.db.Table in_source_table,mycommons.db.connection.ParaConnection in_connection_target,mycommons.db.Table in_target_table){
+
+		//set connection paramagers
+		//this.paraConnectionSouce=in_connection_source;
+		//this.paraConnectionTarget=in_connection_target;
+		//this.tableSource=in_source_table;
+		//this.tableTarget=in_target_table;
+		mycommons.db.connection.ParaConnection paraConnectionSouce=in_connection_source;
 		
-		this.paraConnectionSouce=in_connection_source;
-		this.paraConnectionTarget=in_connection_target;
-		this.tableSource=in_source_table;
-		this.tableTarget=in_target_table;
+		//connect to databases
+		
+		//fetch table and write table
+		
+		//end
+		
 		
 	}
 }
